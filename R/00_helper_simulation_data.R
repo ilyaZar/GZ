a6cdf590d349175f338146f9c3b4f018b3cd3e06
Ilya Zarubin <zarubin@wiso.uni-koeln.de>
2018-12-16 21:10:52 +0100

    changing data generating process functions:
    unclutter code parts where the latent states are simulated in the data generation code parts:
    
    - introduce generic helper for simulation of za, zb,... and xa_t, xb_t,...
    
    - use that instead of previous ugly code repetitions

--- R/00_helper_simulation_data.R
+++ R/00_helper_simulation_data.R
@@ -25,87 +25,27 @@ generate_data <- function(T, K, num_incs,
   dim_reg_p <- length(bet_xp)
   dim_reg_q <- length(bet_xq)
 
-  za <- matrix(rnorm(T*length(bet_xa)), nrow = T, ncol = dim_reg_a)
-  # za[, 1] <- 1
-  par_level_adjust <- za[, -dim_reg_a, drop = FALSE] %*% bet_xa[-dim_reg_a]
-  par_level_adjust <- x_levels[1] * (1 - phi_xa) - par_level_adjust
-  par_level_adjust <- par_level_adjust/bet_xa[dim_reg_a]
-  za[, dim_reg_a]  <- par_level_adjust
+  res_a <- generate_x_z(phi_x = phi_xa, sig_sq_x = sig_sq_xa, bet_x = bet_xa,
+               x_level = x_levels[1], T = T,
+               process_exp = TRUE, intercept = FALSE, x_init = FALSE)
+  xa    <- res_a[[1]]
+  za    <- res_a[[2]]
+  res_b <- generate_x_z(phi_x = phi_xb, sig_sq_x = sig_sq_xb, bet_x = bet_xb,
+                        x_level = x_levels[2], T = T,
+                        process_exp = FALSE, intercept = FALSE, x_init = TRUE)
+  xb    <- res_b[[1]]
+  zb    <- res_b[[2]]
+  res_p <- generate_x_z(phi_x = phi_xp, sig_sq_x = sig_sq_xp, bet_x = bet_xp,
+                        x_level = x_levels[3], T = T,
+                        process_exp = TRUE, intercept = FALSE, x_init = TRUE)
+  xp    <- res_p[[1]]
+  zp    <- res_p[[2]]
+  res_q <- generate_x_z(phi_x = phi_xq, sig_sq_x = sig_sq_xq, bet_x = bet_xq,
+                        x_level = x_levels[4], T = T,
+                        process_exp = FALSE, intercept = FALSE, x_init = TRUE)
+  xq <- res_q[[1]]
+  zq <- res_q[[2]]
 
-  xinit <- 0 # x_levels[1]
-  xa[1] <- f(x_tt = xinit, z = za[1, ], phi_x = phi_xa, bet_x = bet_xa)
-  xa[1] <- xa[1] + sqrt(sig_sq_xa)*rnorm(n = 1)
-
-  for (t in 1:T) {
-    if (t < T) {
-      xa[t + 1] <- f(x_tt = xa[t], z = za[t + 1, ],
-                     phi_x = phi_xa, bet_x = bet_xa)
-      xa[t + 1] <- xa[t + 1] + sqrt(sig_sq_xa)*rnorm(n = 1)
-    }
-  }
-  xa <- exp(xa)
-  zb <- matrix(rnorm(T*length(bet_xb)), nrow = T, ncol = dim_reg_b)
-  # zb[, 1] <- 1
-  par_level_adjust <- zb[, -dim_reg_b, drop = FALSE] %*% bet_xb[-dim_reg_b]
-  par_level_adjust <- x_levels[2] * (1 - phi_xb) - par_level_adjust
-  par_level_adjust <- par_level_adjust/bet_xb[dim_reg_b]
-  zb[, dim_reg_b]  <- par_level_adjust
-
-  xinit <- x_levels[2]
-  xb[1] <- f(x_tt = xinit, z = zb[1, ], phi_x = phi_xb, bet_x = bet_xb)
-  xb[1] <- xb[1] + sqrt(sig_sq_xb)*rnorm(n = 1)
-
-  for (t in 1:T) {
-    if (t < T) {
-      xb[t + 1] <- f(x_tt = xb[t], z = zb[t + 1, ],
-                     phi_x = phi_xb, bet_x = bet_xb)
-      xb[t + 1] <- xb[t + 1] + sqrt(sig_sq_xb)*rnorm(n = 1)
-    }
-  }
-  if (any(xb <= 1)) {
-    stop("xb out of range")
-  }
-  # xb <- rep(150, times = T)
-  zp <- matrix(rnorm(T*length(bet_xp)), nrow = T, ncol = dim_reg_p)
-  # zp[, 1] <- 1
-  par_level_adjust <- zp[, -dim_reg_p, drop = FALSE] %*% bet_xp[-dim_reg_p]
-  par_level_adjust <- x_levels[3] * (1 - phi_xp) - par_level_adjust
-  par_level_adjust <- par_level_adjust/bet_xp[dim_reg_p]
-  zp[, dim_reg_p]  <- par_level_adjust
-
-  xinit <- x_levels[3]
-  xp[1] <- f(x_tt = xinit, z = zp[1, ], phi_x = phi_xp, bet_x = bet_xp)
-  xp[1] <- xp[1] + sqrt(sig_sq_xp)*rnorm(n = 1)
-
-  for (t in 1:T) {
-    if (t < T) {
-      xp[t + 1] <- f(x_tt = xp[t], z = zp[t + 1, ],
-                     phi_x = phi_xp, bet_x = bet_xp)
-      xp[t + 1] <- xp[t + 1] + sqrt(sig_sq_xp)*rnorm(n = 1)
-    }
-  }
-  xp <- exp(xp)
-  # xp <- rep(2.5, times = T)
-  zq <- matrix(rnorm(T*length(bet_xq)), nrow = T, ncol = dim_reg_q)
-  # zq[, 1] <- 1
-  par_level_adjust <- zq[, -dim_reg_q, drop = FALSE] %*% bet_xq[-dim_reg_q]
-  par_level_adjust <- x_levels[4] * (1 - phi_xq) - par_level_adjust
-  par_level_adjust <- par_level_adjust/bet_xq[dim_reg_q]
-  zq[, dim_reg_q]  <- par_level_adjust
-
-  xinit <- x_levels[4]
-  xq[1] <- f(x_tt = xinit, z = zq[1, ], phi_x = phi_xq, bet_x = bet_xq)
-  xq[1] <- xq[1] + sqrt(sig_sq_xq)*rnorm(n = 1)
-
-  for (t in 1:T) {
-    if (t < T) {
-      xq[t + 1] <- f(x_tt = xq[t], z = zq[t + 1, ],
-                     phi_x = phi_xq, bet_x = bet_xq)
-      xq[t + 1] <- xq[t + 1] + sqrt(sig_sq_xq)*rnorm(n = 1)
-    }
-  }
-  # xq <- exp(xq)
-  # xq <- rep(3.5, times = T)
   seq_prob <- rep(seq(from = 0, to = 1 - (1/K), length.out = K), each = T)
   yz <- matrix(qgb2(prob = seq_prob,
                     shape1 = xa,
@@ -131,4 +71,45 @@ parameter_fct_log_norm_test <- function(log_mu, log_sd) {
   exp_mu  <- exp(log_mu + log_sd^2/2)
   exp_var <- (exp(log_sd^2) - 1)*(exp(2*log_mu + log_sd^2))
   return(list(exp_mu, exp_var))
+}
+generate_x_z <- function(phi_x, sig_sq_x, bet_x,
+                         x_level, T,
+                         process_exp, intercept, x_init) {
+  # if (process_exp) {
+  #   x_level <- log(x_level)
+  # }
+  dim_reg <- length(bet_x)
+  x <- rep(0, T)
+
+  z <- matrix(rnorm(T*dim_reg), nrow = T, ncol = dim_reg)
+  if (intercept) {
+    z[, 1] <- 1
+  }
+  par_level_adjust <- z[, -dim_reg, drop = FALSE] %*% bet_x[-dim_reg]
+  par_level_adjust <- x_level * (1 - phi_x) - par_level_adjust
+  par_level_adjust <- par_level_adjust/bet_x[dim_reg]
+  z[, dim_reg]     <- par_level_adjust
+
+  if (x_init == TRUE) {
+    xinit <- x_level
+  } else {
+    xinit <- 0
+  }
+  x[1] <- f(x_tt = xinit, z = z[1, ], phi_x = phi_x, bet_x = bet_x)
+  x[1] <- x[1] + sqrt(sig_sq_x)*rnorm(n = 1)
+
+  for (t in 1:T) {
+    if (t < T) {
+      x[t + 1] <- f(x_tt = x[t], z = z[t + 1, ],
+                    phi_x = phi_x, bet_x = bet_x)
+      x[t + 1] <- x[t + 1] + sqrt(sig_sq_x)*rnorm(n = 1)
+    }
+  }
+  if (process_exp) {
+    x <- exp(x)
+  }
+  if (sum(any(x <= 0)) & process_exp == FALSE) {
+    stop("state process (xa_t, xb_t, xp_t or xq_t) out of range (not positive)")
+  }
+  return(list(x, z))
 }
